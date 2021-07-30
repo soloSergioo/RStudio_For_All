2e08ef9487b534d155dab39552689604edf8aeeb
SergioVlz <sergiovzz99@hotmail.com>
2021-07-30 17:48:07 -0500

    Nuevos Inputs Shiny

--- ShinyEjemplo/server.R
+++ ShinyEjemplo/server.R
@@ -18,6 +18,24 @@ shinyServer(function(input, output) {
     output$range_date_IO <- renderPrint({
         input$range_date_input
     })
+    output$checkbox_IO <- renderPrint({
+        input$single_checkbox
+    })
+    output$checkbox_Group_IO <- renderPrint({
+        input$checkbox_group #devuelve un vector con las selecciones marcadas
+    })
+    output$radiobutton_IO <- renderPrint({
+        input$radio_input #devuelve un solo valor seleccionado
+    })
+    output$ejecutar_IO <- renderPrint({
+        input$boton_ejecutar #boton que funge como un contador para la salida de un valor entero
+    })
+    output$salir_IO <- renderPrint({
+        input$boton_salir #boton que funge como un contador para la salida de un valor entero
+    })
+    output$link_IO <- renderPrint({
+        input$link_seguir
+    })
 
     
 }
--- ShinyEjemplo/ui.R
+++ ShinyEjemplo/ui.R
@@ -19,33 +19,59 @@ shinyUI(fluidPage(
                         animate = TRUE, #hace una caminata de los steps 
                         pre = "$", #caracter antes del valor del slider
                         post = "%"),
-            
+
             sliderInput(inputId = "rangre_slider",
                         label = "Seleccione un rango:",
                         min = 0, 
                         max = 100, 
                         value = c(0,100)),
-            
+           
             numericInput(inputId = "numeric_input",
                          label = "Seleccione la edad:",
                          value = 10,
                          min = 0,
                          max = 150),
-            
+          
             dateInput(inputId = "single_date_input",
                       label = "Ingrese la fecha:",
                       value = today(), #iniciamos con el dia de hoy con la funcion today()
                       min = today()-365, #lo minimo es hoy menos 365 dias atras
                       max = today(), #numero maximo de dias que puede ir hacia adelante
                       language = "es"),
-            
+           
             dateRangeInput(inputId = "range_date_input",
                            label = "Seleccione el rango de fechas:",
                            start = today()-29, #inicia en hoy meos 29 dias el rango
                            end = today(), #finaliza el rango de las fechas
                            format = "dd/mm/yyy", #el formato de las fechas
                            language = "es", #el idioma del calendario
-                           separator = "hasta")
+                           separator = "hasta"),
+            
+            checkboxInput(inputId = "single_checkbox",
+                          label = "Desea recibir informacion",
+                          value = FALSE), #Falso porque la casilla esta deseleccionada
+            
+         
+            checkboxGroupInput(inputId = "checkbox_group",
+                               label = "Seleccione el tipo:",
+                               choices = c("Carro", "Camioneta", "Moto", "Tractor"), #las diferentes selecciones que se pueden tomar
+                               selected = NULL), #Null, quiere decir que no se ha seleccionado nada aun
+            
+            radioButtons(inputId = "radio_input",
+                         label = "Seleccione uno:",
+                         choices = c("Primaria", "Secundaria", "Preparatoria", "Universidad"),
+                         selected = NULL),
+            
+            actionButton(inputId = "boton_ejecutar",
+                         label = "Refrescar"),
+            br(), #instruccion de salto de linea
+            actionButton(inputId = "boton_salir",
+                         label = "Salir"),
+            br(),br(),
+            actionLink(inputId = "link_seguir",
+                       label = "Siguiente"),
+            #DETIENE LA REACTIVIDAD DEL SHINY APP
+            submitButton(text = "Ejecutar") #boton que ejecuta todos los inputs
         ),
        
         mainPanel(
@@ -59,8 +85,20 @@ shinyUI(fluidPage(
             h2("Salida Date Input"),
             verbatimTextOutput("date_IO"),
             h2("Salida Range Date Input"),
-            verbatimTextOutput("range_date_IO")
+            verbatimTextOutput("range_date_IO"),
+            h2("Salida del CheckBox"),
+            verbatimTextOutput("checkbox_IO"),
+            h2("Salida del CheckBox Group"),
+            verbatimTextOutput("checkbox_Group_IO"),
+            h2("Salida del RadioButton"),
+            verbatimTextOutput("radiobutton_IO"),
+            h2("Salida del Boton Ejecutar"),
+            verbatimTextOutput("ejecutar_IO"),
+            h2("Salida del Boton Salir"),
+            verbatimTextOutput("salir_IO"),
+            h2("Salida del Boton Link"),
+            verbatimTextOutput("link_IO")
+        )
         )
     )
-)
 )
