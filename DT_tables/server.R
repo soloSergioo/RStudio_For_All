e23cfbde1e84d1744808dc00e59ec4101787c920
SergioVlz <sergiovzz99@hotmail.com>
2021-07-31 17:31:39 -0500

    Actualizacion

--- DT_tables/server.R
+++ DT_tables/server.R
@@ -9,12 +9,12 @@ shinyServer(function(input, output) {
     output$tabla_1 <- DT::renderDataTable({
         mtcars %>% DT::datatable(rownames = TRUE, #aparece el id de la tabla
                                  filter = "top", #permite que aparezca el filtro como en el .View()
-                                 extensions = "Buttons", #extension de un boton para hacerlo funcionar
+                                 extensions = "Buttons", #extension de un boton para hacerlo funcionar *cuando se agregan los botones se desaparece el show
                                  options = list( #se define la forma en la que se generan los filtros
                                      pageLength = 5, #controla cuantos datos se muestran en la tabla inicialmente
                                      lengthMenu = c(5,10,15,55,-1), #controla el menu desplegable de cuantos datos se muestran *el -1 muestra todas las filas de valores que contiene la tabla
                                      dom = "Bfrtip", #es el tipo de boton que permite descargar la data mostrada
-                                     buttons = c("csv")
+                                     buttons = c("csv") #tipo de descarga csv
                                  ))
     })
     output$tabla_2 <- DT::renderDataTable({
